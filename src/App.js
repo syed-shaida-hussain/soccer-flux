1eb46e03d11dd86cadb65cdeff1199094b0eb3de
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2022-06-08 11:51:45 +0530

    Added homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import { Routes , Route } from "react-router-dom"
+import { Home } from "./Pages/Homepage/homepage";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path = "/home" element = { <Home />} />
+      </Routes>
     </div>
   );
 }
