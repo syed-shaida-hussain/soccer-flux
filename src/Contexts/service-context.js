3ac533eeab22a6620cae61aacd27dbd5766ef896
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2022-06-10 00:06:28 +0530

    Used spread operator for setting video services.

--- src/Contexts/service-context.js
+++ src/Contexts/service-context.js
@@ -6,7 +6,7 @@ import { createContext, useContext, useReducer } from "react";
         case "GET_VIDEOS" : 
             return {...state , videos : (state.videos , action.payload)}
         case "SET_LIKED_VIDEOS" :
-            return {...state , likedVideos : [state.likedVideos , action.payload] }
+            return {...state , likedVideos : [...state.likedVideos , action.payload] }
         case "DELETE_LIKED_VIDEOS" :
             return {...state , likedVideos : (state.likedVideos.filter(video => video._id !== action.payload._id))}
         case "SET_WATCHLATER_VIDEOS" :
@@ -14,7 +14,7 @@ import { createContext, useContext, useReducer } from "react";
         case  "DELETE_WATCHLATER_VIDEOS" :
             return {...state , watchLater : (state.watchLater.filter(video => video._id !== action.payload._id))}
         case "SET_HISTORY_VIDEOS" :
-            return {...state , historyVideos : [state.historyVideos , action.payload] }
+            return {...state , historyVideos : [...state.historyVideos , action.payload] }
         case  "GET_HISTORY_VIDEOS" :
             return {...state , historyVideos : (state.historyVideos , action.payload)}
         case  "DELETE_HISTORY_VIDEO" :
@@ -22,11 +22,11 @@ import { createContext, useContext, useReducer } from "react";
         case  "DELETE_ALL_HISTORY_VIDEOS" :
             return {...state , historyVideos : []}
         case "SET_PLAYLISTS" :
-            return {...state , playlists : [state.playlists , action.payload]}
+            return {...state , playlists : [...state.playlists , action.payload]}
         case "GET_PLAYLIST_VIDEOS" :
             return {...state , playlistVideos : (state.playlistVideos , action.payload)}
         case "SET_PLAYLIST_VIDEOS" :
-            return {...state , playlistVideos : [state.playlistVideos , action.payload]}
+            return {...state , playlistVideos : [...state.playlistVideos , action.payload]}
         default :
             return {...state }
      }
