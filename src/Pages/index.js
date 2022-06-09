08768251022577be4da238e3c79f8746380bf30c
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2022-06-09 11:41:45 +0530

    Added category pages.

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,5 +2,9 @@ import { Signin } from "./Authentication/Login/login"
 import { Signup } from "./Authentication/Signup/signup"
 import { VideoListing } from "./Videolisting/videolisting"
 import { SingleVideoPage } from "./SigleVideoPage/single-video"
+import { CategoryClub } from "./Categories/club-caregory"
+import { CategoryCountry } from "./Categories/country-category"
+import { CategoryGeneral } from "./Categories/general-category"
+import { Home } from "./Homepage/homepage"
 
-export { Signin , Signup , VideoListing , SingleVideoPage}
+export { Signin , Signup , VideoListing , SingleVideoPage , CategoryClub , CategoryCountry , CategoryGeneral , Home}
