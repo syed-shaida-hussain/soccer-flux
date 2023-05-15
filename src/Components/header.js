7bf8be93907a1987fca736d98ca8b1c5c73a5e0c
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2023-05-15 11:27:57 +0530

    fix : fixed style

--- src/Components/header.js
+++ src/Components/header.js
@@ -23,7 +23,7 @@ const Header = () => {
     <header>
         <ul className = "nav-links"> 
             <li className = "nav-pill flex ctr-vert button" onClick={() => navigate("/home")}><span className="material-symbols-outlined hero-icon">play_circle</span>Soccer Flux</li>
-            {location.pathname === "/home" && <input type="text" className = "search-bar" placeholder = "Search" onChange={searchbarChangeHandler}/>}
+            {location.pathname === "/home" | location.pathname === "/" && <input type="text" className = "search-bar" placeholder = "Search" onChange={searchbarChangeHandler}/>}
             <li className = "nav-pill login-btn" > {!auth.status ? <Link className = "nav-pill login-btn" to = "/login"><button className="logout-btn" onClick={() => navigate("/login")}>Sign in</button></Link> : <button className="logout-btn" onClick={() => logout()}>Logout</button>} 
             </li>
         </ul>
